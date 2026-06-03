27a2bcbaacd1353e1604cf93c228137e361ac945
Supremacy001 <117300968+Supremacy001@users.noreply.github.com>
2026-06-03 20:48:21 +0530

    Initial Commit

---
+++ Loops/iterations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout<<"enter number of iterations: ";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "This is the " << i << "'th iteration" << endl;
+    }
+    return 0;
+}
