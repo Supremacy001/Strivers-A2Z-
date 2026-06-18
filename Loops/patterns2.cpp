af67cb4a456d1576f18dd4c0a48c3bb57d120a35
Supremacy001 <117300968+Supremacy001@users.noreply.github.com>
2026-06-18 13:55:30 +0530

    optimized inverted pyramid

--- Loops/patterns2.cpp
+++ Loops/patterns2.cpp
@@ -47,28 +47,19 @@ void starpyramid(int n) {
 }
 
 void invertedstarpyramid (int n) {
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
         {
             cout<<"  ";
         }
         
-        for (int k = n; k >= i ; k--)
+        for (int k = 0; k < 2*n - (2*i + 1); k++)
         {
             cout<<" *";
+        }
 
-        }
-        
-        for (int m = n-1; m >= i; m--)
-        {
-            cout<<" *";
-        }
-        
-        
-        cout<<endl;
-        
-        
+        cout<<endl;   
     }
     
 }
