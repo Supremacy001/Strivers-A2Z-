d27b2c871b629018a67022530f01196d8f901c18
Supremacy001 <117300968+Supremacy001@users.noreply.github.com>
2026-06-19 13:13:32 +0530

    half diamond

--- Loops/patterns3.cpp
+++ Loops/patterns3.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
 using namespace std;
 
-voi
+void halfdiamond(int n) {
+
+    for (int i = 0; i <= 2*n - 1; i++)
+    {
+        int stars;
+
+        if (i <= n)
+        {
+            stars = i;
+        }
+
+        else {
+            stars = 2*n - i;
+        }
+
+        for (int j = 0; j < stars; j++)
+        {
+            cout<<" *";
+        }
+        cout<<endl;
+         
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+
+    cout<<"enter no. of rows: ";
+    cin>>n;
+
+    halfdiamond(n);
+    return 0;
+}
